49c21801c99b4be1a51710fcccd1fb0f286828a8
Carlos Rocha <carlos.rocha58@gmail.com>
2018-03-26 23:36:50 -0500

    remove lodash

--- src/utils.js
+++ src/utils.js
@@ -1,11 +1,24 @@
 // @flow
 
 import orderBy from 'lodash/orderBy';
-import some from 'lodash/some';
 import type { SortBy, Row, Value, Filters } from './types';
 
 export function sort({ prop, order }: SortBy, data: Row[]) {
   return orderBy(data, prop, order == 'descending' ? 'desc' : 'asc');
+}
+
+function some(data: Row, f: Value => boolean) {
+  if (Array.isArray(data)) {
+    return data.some(f);
+  } else {
+    for (let key of Object.keys(data)) {
+      if (f(data[key])) {
+        return true;
+      }
+    }
+
+    return false;
+  }
 }
 
 export function filter(filters: Filters, filterValues: Object, data: Row[]) {
@@ -17,8 +30,8 @@ export function filter(filters: Filters, filterValues: Object, data: Row[]) {
 
       if (
         (!prop && some(row, filter.bind(null, value))) ||
-        // $FlowFixMe
-        filter(value, row[key])
+        // $FlowFixMe: Property accessor should work with both number and string
+        filter(value, row[prop])
       ) {
         return true;
       }
